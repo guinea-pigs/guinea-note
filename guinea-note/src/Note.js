9de1a3b279e624ad32d0542af2a2ff05ebf4ff7c
abigailbarrera <abigailbarrera89@outlook.com>
2018-07-01 23:52:34 -0700

    Took away mongoose and used Firebase instead

---
+++ guinea-note/src/Note.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import AllNotes from './AllNotes.js'
+
+import { connect } from 'react-redux';
+
+import firebase from './firebase';
+import { Redirect } from 'react-router-dom';
+
+import withAuthorization from './withAuthorization';
+
+
+import generateId from './utils';
+class Note extends Component {
+
+    componentDidMount() {
+        const ref = firebase.database().ref('users/');
+
+        this.props.dispatch({ type: 'LOADING_TRUE' });
+        ref.on('value', snapshot => {
+            if (snapshot.val() === null) {
+                this.props.dispatch({ type: 'LOADING_FALSE' });
+                return;
+            }
+            [...Object.values(snapshot.val())].map((note) => {
+                this.props.dispatch({ type: 'ADD_NOTE', data: note })
+                this.props.dispatch({ type: 'LOADING_FALSE' })
+            })
+        })
+
+    }
+
+
+
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const title = this.titleInput.value;
+        this.props.dispatch({ type: 'NO_ERROR_RECEIVED' })
+        //validations
+        const message = this.messageInput.value;
+        //generate id
+        const id = generateId();
+        const newNote = {
+            id,
+            title,
+            message,
+            editing: false,
+            errorMessage: ''
+        }
+        const noteRef1 = firebase.database().ref('users/')
+        const noteKey = noteRef1.push()
+        const noteRef = firebase.database().ref('users/' + noteKey.key)
+        const uid = firebase.auth().currentUser.uid
+        noteRef.set({
+            id: id,
+            title: title,
+            message: message,
+            editing: false,
+            uid: uid,
+            key: noteKey.key,
+            errorMessage: ''
+        })
+
+        if (this.props.notes.editing) {
+            this.props.dispatch({
+                type: 'ADD_EDIT_POST',
+                data: newNote
+            })
+        }
+        this.props.dispatch({
+            type: 'ADDING_NOTE'
+
+        })
+        this.titleInput.value = '';
+        this.messageInput.value = '';
+
+
+    }
+
+    handleLogout = () => {
+        firebase.auth().signOut().then(() => {
+            <Redirect to="/" />
+        }).catch(() => {
+            console.log('Error happened')
+        })
+        localStorage.removeItem('uid');
+    }
+    render() {
+        return (
+            <div className="post-grid">
+                <div className="post-container">
+                    <div className="logout-container">
+                        <h2 className="righter"></h2>
+                        <button className="logout" onClick={this.handleLogout}>Logout</button>
+                    </div>
+                    <div className="first-stuff">
+                        <h2 className="post_heading">Note Content</h2>
+
+                    </div>
+                    <form className="form" onSubmit={this.handleSubmit}>
+                        <input required type="text" ref={(input) => this.titleInput = input} placeholder="Enter Title for Note"
+                        /><br /><br />
+                        <textarea required ref={(input) => this.messageInput = input} placeholder="Enter the Note" cols="28" rows="5"></textarea><br />
+                        <button>Post Note</button>
+
+                    </form>
+                    {this.props.errors ? <p style={{ color: '#ff7777' }}>{this.props.errors.message}</p> : null}
+                </div>
+                <AllNotes notes={this.props.notes} />
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    notes: state.notes,
+    errors: state.errors
+
+})
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(connect(mapStateToProps)(Note));
--- guinea-note/src/index.js
+++ guinea-note/src/index.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Edit from './components/Edit';
-import Create from './components/Create';
-import Show from './components/Show';
+
+import noteReducer from './reducers/noteReducer';
+import errorReducer from './reducers/errorReducer';
+import loadingReducer from './reducers/loadingReducer';
+import authReducer from './reducers/authReducer';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+
+
+
+const reducer = combineReducers({
+    notes: noteReducer,
+    errors: errorReducer,
+    loading: loadingReducer,
+    authed: authReducer,
+})
+
+const store = createStore(reducer, applyMiddleware(ReduxThunk));
+
+
+
 
 ReactDOM.render(
-  <Router>
-      <div>
-        <Route exact path='/' component={App} />
-        <Route path='/edit/:id' component={Edit} />
-        <Route path='/create' component={Create} />
-        <Route path='/show/:id' component={Show} />
-      </div>
-  </Router>,
-  document.getElementById('root')
-);
-registerServiceWorker();
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
